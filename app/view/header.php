6c1122b79a5b3871da264a5fa3d40d6899506183
Jose <jose@ejemplo.com>
2026-02-06 19:08:46 -0600

    no funciona el menu

--- app/view/header.php
+++ app/view/header.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>San José</title>
+
      <link rel="icon" type="image/png" href="/public//assets//logo-pestaña.png">
     <link rel="stylesheet" href="/public/cssB/bootstrap.min.css">
     <link href="/public/cssB//bootstrap-icons-1.10.5/font//bootstrap-icons.css" rel="stylesheet">
--- app/view/home.php
+++ app/view/home.php
@@ -1,10 +1,5 @@
 <?php include 'header.php'; ?>
 
-<link rel="stylesheet" href="<?= $base ?>/public/cssB/bootstrap.min.css">
-<link href="<?= $base ?>/public/cssB/bootstrap-icons-1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-<link rel="stylesheet" href="<?= $base ?>/public/css/styles.css">
-<link rel="icon" type="image/png" href="<?= $base ?>/public/assets/logo-pestaña.png">
-
 <style>
 /* En Home, reducir el margen inferior del navbar */
 nav.navbar { margin-bottom: 0rem !important; }
